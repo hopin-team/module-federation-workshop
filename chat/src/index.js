e1548e6eef05bab5bc706945fdc78ecb735f2db2
Alex Lobera <alex.lobera@hopin.to>
2021-05-24 12:44:23 +0100

    update main

--- chat/src/index.js
+++ chat/src/index.js
@@ -1,5 +1,10 @@
 import * as gen from "txtgen";
 
+const root = document.getElementById("root-chat");
+
+const h1 = document.createElement("h1");
+h1.append("What attendees are saying");
+
 const ul = document.createElement("ul");
 for (let index = 0; index < 5; index++) {
   const li = document.createElement("li");
@@ -7,4 +12,5 @@ for (let index = 0; index < 5; index++) {
   ul.append(li);
 }
 
-document.getElementById("root-chat").append(ul);
+root.append(h1);
+root.append(ul);
