4f18b0c4892990e5e7846944e9321cf0658313bf
Alex Lobera <alex.lobera@hopin.to>
2021-06-13 09:29:43 +0100

    wip

--- host/src/bootstrap.js
+++ host/src/bootstrap.js
@@ -3,6 +3,6 @@ import mount from "chat/App";
 
 const title = document.createElement("h1");
 title.append("Event app");
-document.getElementById("root-container").prepend(title);
+document.getElementById("root-host").prepend(title);
 
 mount(document.getElementById("root-chat"));
