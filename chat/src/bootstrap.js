a40f0cea62c6091a71f97c9978a8cb358fdbd38f
Alex Lobera <alex.lobera@hopin.to>
2021-06-14 10:05:06 +0100

    update chat/src/bootstrap.js

--- chat/src/bootstrap.js
+++ chat/src/bootstrap.js
@@ -1,28 +1,20 @@
 import ReactDOM from "react-dom";
-import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./components/App";
 
-function mount(el, { onNavigate, history = createMemoryHistory() } = {}) {
-  const cleanups = [];
-  if (onNavigate) cleanups.push(history.listen((e) => onNavigate(e.pathname)));
-  if (el) ReactDOM.render(<App history={history} />, el);
+function mount(el) {
+  if (el) ReactDOM.render(<App />, el);
 
   return {
-    onHostNavigate: (pathname) => {
-      const currentPathname = history.location.pathname;
-      if (currentPathname !== pathname) history.push(pathname);
-    },
     unmount: () => {
-      cleanups.forEach((cleanup) => cleanup());
       ReactDOM.unmountComponentAtNode(el);
     },
   };
 }
 
-if (process.env.NODE_ENV === "development" && typeof document !== "undefined") {
+if (process.env.NODE_ENV === "development") {
   const el = document.getElementById("root-chat-dev");
   if (el) {
-    mount(el, { history: createBrowserHistory() });
+    mount(el);
   }
 }
 
