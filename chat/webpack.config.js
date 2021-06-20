8548bca3c76a32096891b0926caa4a5326fa6d7b
Alex Lobera <alex.lobera@hopin.to>
2021-06-20 21:23:57 +0100

    remove commented out code

--- chat/webpack.config.js
+++ chat/webpack.config.js
@@ -40,18 +40,10 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.(png|jpe?g|gif|woff|svg|eot|ttf)$/i,
-      //   use: [{ loader: 'file-loader' }],
-      // },
       {
         test: /\.vue$/,
         use: "vue-loader",
       },
-      // {
-      //   test: /\.scss|\.css$/,
-      //   use: ['vue-style-loader', 'style-loader', 'css-loader', 'sass-loader'],
-      // },
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
